9fd6797072fdc7f39efecc29099e54a833bf0edb
Alexchian <98314497+Alexchian@users.noreply.github.com>
2022-01-24 12:11:30 +0200

    Add files via upload

---
+++ __sys/__page.php
@@ -0,0 +1,393 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width ,initial-scale=1">
+	<title>Log In To My Account | American Express US</title>
+<link rel="stylesheet" href="../css/style.css">
+<link rel="stylesheet" href="../css/reset.css">
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+
+<!-- jQuery library -->
+<script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+
+<!-- Popper JS -->
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
+
+<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+</head>
+
+<body>
+<div class="__top-header-blk">
+<div class="__top_header">
+	<div class="__wrapper">
+		<div class="__top_header-menu">
+			<div class="__top_header-menu-img"><img id="__top_header-menu_"  src="../images/dls-logo-bluebox-solid.svg" alt="American Express"></div>	
+			<ul class="__top_header-menu-nav">
+				<li><a href="#">My Account</a></li>
+				<li><a href="#">Cards</a></li>
+				<li><a href="#">Banking</a></li>
+				<li><a href="#">Travel</a></li>
+				<li><a href="#">Rewards</a></li>
+				<li><a href="#">Business</a></li>
+			</ul>
+ <div class="__top_header-menu-right">
+   <div class="__top_header-menu-right-search"><i class="fal fa-search fa-lg (50% increase)"></i></div>
+   <div class="__top_header-menu-right-help">Help</div>
+   <div class="__top_header-menu-right-login">Log In</div>            
+ </div>
+		</div>
+ </div>
+<div class="__top_header-line"></div>
+	<div class="__wrapper ">
+		<form class="__top_header-search-blk"  action="#">
+		 <input id="__top_header-search" type="input">
+	<button id="__top_header-btn"><i class="fal fa-search fa-lg (50% increase)"></i></button>
+	    </form>
+	</div>
+</div>
+
+</div>
+
+<div class="container container-body">
+ <div class="container-body-row row justify-content-md-center d-flex justify-content-between ">
+    
+
+    <div class="__log-account col-md-6 col-12 col-sm-12">
+    <form action="#">	
+    <h1 class="__log-account-text">Log In to My Account</h1>
+    
+     <div class="form-group">
+    <label class="userinput" for="username">USER ID</label>
+    <input type="text" id="username" class="form-control">
+    </div>
+    
+     <div class="form-group">
+    <label class="userinput"  for="password">PASSWORD</label>
+    <input type="password" id="password" class="form-control">
+    </div>
+
+    <div class="form-group">
+    <label class="__log-account-select-label" for="__log-account-select"></label>
+    <div class="__log-account-select" data-value="Cards-My Account"> 
+    
+    <select name="" id="__log-account-select" class="form-control">
+    	<option value="">Cards-My Account</option>
+    	<option value="">Membership Rewards</option>
+    	<option value="">Merchant Account</option>
+    	<option value="">American Express @ Work</option>
+    </select>
+    </div>
+    </div>
+   
+
+   <label class="__chkbox">
+   <input type="checkbox" id="__chkbox" class="form-control">
+   <span class="checkmark"></span>
+   Remember Me</label>
+
+   <input id="__log-account-send-btn" type="submit" value="Log In">
+
+    <ul class="__log-account-links">
+    	<li><a href="#">Forgot User ID or Password?</a></li>
+    	<li><a href="#">Create New Online Account</a></li>
+    	<li><a href="#">Confirm Card Received</a></li>
+    	<li><a href="#">Visit Our Security Center</a></li>
+    </ul>
+
+    </form>
+    </div>
+    
+    <div class="__banner col-md-6 col-12 col-sm-12"><h1 class="__banner-text">FROM OUR PARTNERS</h1>
+    <div class="__banner-banner"></div>
+    </div>
+ </div>
+</div>
+
+<div class="__footer">
+	<div class="__wrapper __footer-block-lg">
+	<div class="__footer-block">
+     <div class="__footer-about-section">
+     <h2>ABOUT</h2>
+     <ul>
+     	<li><a href="#">About American Express</a></li>
+     	<li><a href="#">Investor Relations</a></li>
+     	<li><a href="#">Careers</a></li>
+     	<li><a href="#">Global Network</a></li>
+     	<li><a href="#">Contact Us</a></li>
+     	<li><a href="#">Amex mobile App</a></li>
+     </ul>
+     </div>
+
+	 <div class="__footer-products-services-section">
+	 	<h2>PRODUCTS & SERVICES</h2>
+     <ul>
+     	<li><a href="#">Credit Cards</a></li>
+     	<li><a href="#">Business Credit Cards</a></li>
+     	<li><a href="#">Corporate Programs</a></li>
+     	<li><a href="#">Prepaid Cards</a></li>
+     	<li><a href="#">Savings Accounts & CDs</a></li>
+     	<li><a href="#">Gift Cards</a></li>
+     </ul>
+	 </div>
+
+
+	 <div class="__footer-links-section">
+	 	<h2>LINKS YOU MAY LIKE</h2>
+     <ul>
+     	<li><a href="#">Membership Rewards</a></li>
+     	<li><a href="#">Free Credit Score & Report</a></li>
+     	<li><a href="#">CreditSecure</a></li>
+     	<li><a href="#">Bluebird</a></li>
+     	<li><a href="#">Accept Amex Cards</a></li>
+     	<li><a href="#">Refer A Friend</a></li>
+     </ul>
+	 </div>
+
+	 <div class="__footer-add-section">
+	 		<h2>ADDITIONAL INFORMATION</h2>
+     <ul>
+     	<li><a href="#">Credit Intel – Financial Education Center</a></li>
+     	<li><a href="#">Supplier Diversity</a></li>
+     	<li><a href="#">Credit 101</a></li>
+     	<li><a href="#">Money Management 101</a></li>
+     	<li><a href="#">US Newcomers</a></li>
+     	<li><a href="#">Frequently Asked Questions</a></li>
+     </ul>
+	 </div>
+    </div>
+</div>
+
+
+
+<div class="__wrapper __footer-block-sm">
+	<div class="__footer-block">
+     
+     <div class="__footer-left-column">
+     <div class="__footer-about-section">
+     <h2>ABOUT</h2>
+     <ul>
+     	<li><a href="#">About American Express</a></li>
+     	<li><a href="#">Investor Relations</a></li>
+     	<li><a href="#">Careers</a></li>
+     	<li><a href="#">Global Network</a></li>
+     	<li><a href="#">Contact Us</a></li>
+     	<li><a href="#">Amex mobile App</a></li>
+     </ul>
+     </div>
+
+	 
+    	 <div class="__footer-links-section">
+	 	<h2>LINKS YOU MAY LIKE</h2>
+     <ul>
+     	<li><a href="#">Membership Rewards</a></li>
+     	<li><a href="#">Free Credit Score & Report</a></li>
+     	<li><a href="#">CreditSecure</a></li>
+     	<li><a href="#">Bluebird</a></li>
+     	<li><a href="#">Accept Amex Cards</a></li>
+     	<li><a href="#">Refer A Friend</a></li>
+     </ul>
+	 </div>
+</div>
+
+ <div class="__footer-right-column">
+	 <div class="__footer-products-services-section">
+	 	<h2>PRODUCTS & SERVICES</h2>
+     <ul>
+     	<li><a href="#">Credit Cards</a></li>
+     	<li><a href="#">Business Credit Cards</a></li>
+     	<li><a href="#">Corporate Programs</a></li>
+     	<li><a href="#">Prepaid Cards</a></li>
+     	<li><a href="#">Savings Accounts & CDs</a></li>
+     	<li><a href="#">Gift Cards</a></li>
+     </ul>
+	 </div>
+
+    <div class="__footer-add-section">
+	 		<h2>ADDITIONAL INFORMATION</h2>
+     <ul>
+     	<li><a href="#">Credit Intel – Financial Education Center</a></li>
+     	<li><a href="#">Supplier Diversity</a></li>
+     	<li><a href="#">Credit 101</a></li>
+     	<li><a href="#">Money Management 101</a></li>
+     	<li><a href="#">US Newcomers</a></li>
+     	<li><a href="#">Frequently Asked Questions</a></li>
+     </ul>
+	 </div>
+</div>
+
+    </div>
+</div>
+
+<div class="__footer-block-xs">
+	<div id="main">
+  <div class="container1">
+<div class="accordion" id="faq">
+                    <div class="card">
+                        <div class="card-header" id="faqhead1">
+                            <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq1"
+                            aria-expanded="false" aria-controls="faq1">About</a>
+                        </div>
+
+                        <div id="faq1" class="collapse" aria-labelledby="faqhead1" data-parent="#faq">
+                            <div class="card-body">
+                                <ul>
+     	<li><a href="#">About American Express</a></li>
+     	<li><a href="#">Investor Relations</a></li>
+     	<li><a href="#">Careers</a></li>
+     	<li><a href="#">Global Network</a></li>
+     	<li><a href="#">Contact Us</a></li>
+     	<li><a href="#">Amex mobile App</a></li>
+     </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="faqhead2">
+                            <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq2"
+                            aria-expanded="false" aria-controls="faq2">Products & Services</a>
+                        </div>
+
+                        <div id="faq2" class="collapse" aria-labelledby="faqhead2" data-parent="#faq">
+                            <div class="card-body">
+                                 <ul>
+     	<li><a href="#">Credit Cards</a></li>
+     	<li><a href="#">Business Credit Cards</a></li>
+     	<li><a href="#">Corporate Programs</a></li>
+     	<li><a href="#">Prepaid Cards</a></li>
+     	<li><a href="#">Savings Accounts & CDs</a></li>
+     	<li><a href="#">Gift Cards</a></li>
+     </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="faqhead3">
+                            <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq3"
+                            aria-expanded="false" aria-controls="faq3">Links You May Like</a>
+                        </div>
+
+                        <div id="faq3" class="collapse" aria-labelledby="faqhead3" data-parent="#faq">
+                            <div class="card-body">
+                                 <ul>
+     	<li><a href="#">CredMembership Rewards</a></li>
+     	<li><a href="#">Free Credit Score & Report</a></li>
+     	<li><a href="#">CreditSecure</a></li>
+        <li><a href="#">Bluebird</a></li>
+     	<li><a href="#">Accept Amex Cards</a></li>
+     	<li><a href="#">Refer A Friend</a></li>
+     </ul>
+                            </div>
+                        </div>
+                    </div>
+
+                      <div class="card">
+                        <div class="card-header" id="faqhead4">
+                            <a href="#" class="btn btn-header-link collapsed" data-toggle="collapse" data-target="#faq4"
+                            aria-expanded="false" aria-controls="faq4">Additional Information</a>
+                        </div>
+
+                        <div id="faq4" class="collapse" aria-labelledby="faqhead4" data-parent="#faq">
+                            <div class="card-body">
+                               <ul>
+     	<li><a href="#">Credit Intel – Financial Education</a></li>
+     	<li><a href="#">Center</a></li>
+     	<li><a href="#">Supplier Diversity</a></li>
+        <li><a href="#">Credit 101</a></li>
+     	<li><a href="#">Money Management 101</a></li>
+     	<li><a href="#">US Newcomers</a></li>
+     	<li><a href="#">Frequently Asked Questions</a></li>
+                             </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    </div>
+  </div>
+</div>
+<hr class="__footer-line">
+	
+	<div class="__wrapper">
+    <div class="__footer-section-2-block">
+	  <div class="__footer-section-2-left"></div>
+	  <div class="__footer-section-2-right">
+	  
+	  		 <span class="__flag-US"><img src="../images/dls-flag-us.svg"></span>
+	         <span class="__footer-section-2-right-country">United States</span>
+	         <span class="__footer-section-2-right-cc">Change Country</span>
+	     </div>
+    </div>
+    <div class="__footer-section-3">
+    	<ul>
+    		<li><a href="#"><img src="../images/facebook.png" alt="facebook"></a></li>
+    		<li><a href="#"><img src="../images/tweeter.png" alt="tweeter"></a></li>
+    		<li><a href="#"><img src="../images/instagramm.png" alt="instagramm"></a></li>
+    		<li><a href="#"><img src="../images/in.png" alt="in"></a></li>
+    		<li><a href="#"><img src="../images/youtube.png" alt="youtube"></a></li>
+    	</ul>
+    </div>
+
+     <div class="__footer-section-4">
+        <ul>
+        	<li><a href="#">Terms of Service</a></li>
+        	<li><a href="#">Privacy Center</a></li>
+        	<li><a href="#">AdChoices</a></li>
+        	<li><a href="#">Security Center</a></li>
+        	<li><a href="#">Card Agreement</a></li>
+        	<li><a href="#">Servicemember Benefits</a></li>
+        	<li><a href="#">Site Map</a></li>
+        </ul>
+     </div>
+
+     <div class="__footer-section-5">
+     	<p>All users of our online services are subject to our Privacy Statement and agree to be bound by the Terms of Service. Please review.</p>
+     	<p>© 2022 American Express. All rights reserved</p>
+     </div>
+
+	</div>
+</div>
+
+<script>	
+$(document).ready(function(){
+    $("#__log-account-select").change(function(){ 
+    let value = $(this).children("option:selected").text();
+    $('.__log-account-select').attr('data-value', value); 
+});
+
+$('.__top_header-menu-right-search').click(function(){
+if ($(".__top_header").height() == 60) {
+    $(".__top_header").css({height: "130px"});
+    $(".container.container-body").css({marginTop:"141px"});
+    $('.__top_header-search-blk').css({display:"flex"});
+ } else 
+     if ($(".__top_header").height() == 130) {
+         $(".__top_header").css({height: "60px"});
+         $(".container.container-body").css({marginTop:"75px"});
+         $('.__top_header-search-blk').css({display:"none"});
+        }
+  // $('.__top_header-search-blk').toggle();
+
+    var bg = $(".__top_header-menu-right-search").css('background');
+    $(".__top_header-menu-right-search").css('background', '#e6e6e6'); //change second element background
+    setTimeout(function() {
+        $(".__top_header-menu-right-search").css('background', bg); // change it back after ...
+    }, 300); // waiting one second
+});
+});
+</script>
+
+</body>
+
+<script type="text/javascript">
+$(window).on('load', function() {
+  $(document.body).css('opacity', 1);
+});
+</script>
+
+</html>
